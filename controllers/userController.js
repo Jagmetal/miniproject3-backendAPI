f91bf17ee6dc5855d77f00c22320237be92592d5
Davinder <davblogger@gmail.com>
2023-08-30 14:10:22 +1200

    Optimise the UserController

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,18 +4,16 @@ let Models = require("../models"); //matches index.js
 const getUsers = (res) => {
     //finds all users
     Models.User.find({})
-        .then(usersData => res.send({ result: 200, data: data }))
+        .then(users => res.send(users))
         .catch(err => {
             console.log(err);
             res.send({ result: 500, error: err.message })
         })
 }
 
-const createUser = (data, res) => {
-    //creates a new user using JSON data POSTed in request body
-    console.log(data)
-    new Models.User(data).save()
-        .then(data => res.send({ result: 200, data: data }))
+const createUser = (userPayload, res) => {
+    new Models.User(userPayload).save()
+        .then(user => res.send(user))
         .catch(err => {
             console.log(err);
             res.send({ result: 500, error: err.message })
